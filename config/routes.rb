945ee4513978702c0cd6e4311ae604584bb33349
Blaine <b.atsbeha@gmail.com>
2020-11-29 21:16:08 -0800

    created Omniauth routes & began current user routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Omniauth Login Route
+  get "/auth/github", as: "github_login"
+
+  # Omniauth Github Callback Route
+  get "/auth/:provider/callback", to: "user#create"
+
   root "works#root"
-  get "/login", to: "users#login_form", as: "login"
-  post "/login", to: "users#login"
-  post "/logout", to: "users#logout", as: "logout"
+  # get "/login", to: "users#login_form", as: "login"
+  # post "/login", to: "users#login"
+  # post "/logout", to: "users#logout", as: "logout"
+  get "/user/current", to: "user#current", as: "current_user"
 
   resources :works
   post "/works/:id/upvote", to: "works#upvote", as: "upvote"
